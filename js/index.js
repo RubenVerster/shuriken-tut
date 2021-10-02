23d5a5664999d8696f42f8a7f2ace3908f23c4e0
ruben.verster <ruben.verster@1-grid.com>
2021-10-02 08:46:37 +0200

    render shuriken on click

--- js/index.js
+++ js/index.js
@@ -2,4 +2,19 @@ let container = document.querySelector('.container');
 let windowHeight = window.innerHeight;
 let windowWidth = window.innerWidth;
 
-container.addEventListener('click', () => console.log('clicked'));
+const generateRandomHeight = () => {
+  let rand = Math.random();
+  return rand * windowHeight;
+};
+
+const renderShuriken = () => {
+  shuriken = document.createElement('img');
+  shuriken.src = '../img/shuriken-picture.png';
+  shuriken.className = 'throw-shuriken';
+  shuriken.style.animation = 'throw-shuriken forwards 2s';
+  shuriken.style.top = `${generateRandomHeight()}px`;
+
+  container.appendChild(shuriken);
+};
+
+container.addEventListener('click', () => renderShuriken());
