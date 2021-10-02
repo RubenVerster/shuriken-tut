ec06377839019b6cde47345b24a51054ec38608a
ruben.verster <ruben.verster@1-grid.com>
2021-10-02 08:31:52 +0200

    javascript files

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let container = document.querySelector('.container');
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
+
+container.addEventListener('click', () => console.log('clicked'));
